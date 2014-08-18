46cd13571f921caf15103d907eb0ffda8721ba66
Brend Wanders <b.wanders@utwente.nl>
2014-08-18 15:07:48 +0200

    Adds actual example of life graph updates.

---
+++ graph_sample.py
@@ -0,0 +1,31 @@
+from eca import *
+
+import time
+import random
+
+root_content_path = 'graph-sample'
+
+@event('init')
+def setup(ctx, e):
+    fire('sample', {'previous': 0.0})
+
+def clip(lower, value, upper):
+    return max(lower, min(value, upper))
+
+@event('sample')
+def generate_sample(ctx, e):
+    # base sample on previous one
+    sample = clip(e.previous + random.uniform(+5.0, -5.0))
+
+    # emit to outside world
+    emit('sample',{
+        'value': sample
+    })
+
+    # this will halt the execution of other rules in this context
+    time.sleep(0.05)
+
+    # chain event
+    fire('sample',{
+        'previous': sample
+    })
