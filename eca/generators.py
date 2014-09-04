f324016538a05e4511b051f044cfded4688e90f7
Brend Wanders <b.wanders@utwente.nl>
2014-09-04 13:31:22 +0200

    Refactors tweet generator to generic generator.

--- eca/generators.py
+++ eca/generators.py
@@ -6,51 +6,97 @@ from . import fire, get_context, context_switch, register_auxiliary, auxiliary
 import logging
 import sys
 
+
 logger = logging.getLogger(__name__)
 
-class OfflineTweetGenerator:
-    def __init__(self, context, data_file, time_factor=1000.0, event_name='tweet'):
+class EventGenerator:
+    """
+    An event generator uses a generation function to generate events from
+    any external source.
+    """
+    def __init__(self, context, generator, event_name='tweet', **kwargs):
         self.context = context
-        self.data_file = data_file
-        self.time_factor = time_factor
         self.event_name = event_name
-
+        self.generator = generator
+        self.generator_args = kwargs
+        self.stop_flag = threading.Event()
 
     def start(self):
+        """
+        Starts a thread to handle run this generator.
+        """
         thread = threading.Thread(target=self.run)
         thread.start()
 
+    def stop(self):
+        """
+        Requests shutdown of generator.
+        """
+        self.stop_flag.set()
 
     def run(self):
-        logger.debug("Running tweet event generator")
-        try:
-            with context_switch(self.context), open(self.data_file) as data:
-                for event in self.generate_events(data):
-                    fire(self.event_name, event)
-        except IOError as e:
-            print(e, file=sys.stderr)
-            logger.error("Is the tweet data downloaded?")
+        """
+        Invoke the generator to get a sequence of events.
 
-       
-    def generate_events(self, data):
+        This method passes an event to the generator which will be set to True
+        if the generator should terminate. Immediate termination is not required.
+        """
+        logger.debug("Running event generator")
+        with context_switch(self.context):
+            for event in self.generator(self.stop_flag, **self.generator_args):
+                fire(self.event_name, event)
+
+    
+def offline_tweets(stop, data_file, time_factor=1000):
+    """
+    Offline tweet replay.
+
+    Takes a datafile formatted with 1 tweet per line, and generates a sequence of
+    scaled realtime items.
+    """
+
+    # timing functions return false if we need to abort
+    def delayer(duration):
+        logger.debug("Delay for next tweet {}s ({}s real)".format(delay, delay/time_factor))
+        return not stop.wait(delay / time_factor)
+
+    def immediate(duration):
+        return not stop.is_set()
+
+    # select timing function based on time_factor
+    delayed = immediate if time_factor is None else delayer
+
+    with open(data_file) as data:
         last_time = None
+        lines = 0
         for line in data:
-            tweet = json.loads(line)
+            lines += 1
+
+            try:
+                tweet = json.loads(line)
+            except ValueError as e:
+                logger.error("Could not read tweet on {}:{} (reason: {})".format(data_file,lines, e))
+                continue
+
+            # time scale the tweet
             tweet_time = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y')
+
             if not last_time:
-                logger.debug("Initialised last time at {}".format(tweet_time))
                 last_time = tweet_time
             
             wait = tweet_time - last_time 
-            delay = wait.total_seconds() / self.time_factor
+            delay = wait.total_seconds()
+   
+            # delay and yield or break depending on success
+            if delayed(delay):
+                yield tweet
+            else:
+                break
 
-            logger.debug("Delay for next tweet {}s".format(delay))
-            time.sleep(delay)
-            yield tweet
-
+      
 def start_offline_tweets(data_file, name='tweeter', **kwargs):
     context = get_context()
     if context is None:
         raise NotImplementedError("Can not start offline tweet replay outside of a context.")
-    register_auxiliary(name, OfflineTweetGenerator(get_context(), data_file, **kwargs))
+    register_auxiliary(name, EventGenerator(context, generator=offline_tweets, data_file=data_file, **kwargs))
     auxiliary(name).start()
