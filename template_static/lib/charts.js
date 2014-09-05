4f2baaea15660c0aab4e01f38ce62cd1be506370
Brend Wanders <b.wanders@utwente.nl>
2014-09-05 16:48:15 +0200

    Adds documentation to charts.js.

--- template_static/lib/charts.js
+++ template_static/lib/charts.js
@@ -2,13 +2,16 @@
 
 // a simple rolling chart with memory
 block.fn.rolling_chart = function(config) {
+    // combine default configuration with user configuration
     var options = $.extend({
         memory: 100,
         data: []
     }, config);
 
+    // maintain state for this block
     var data = options.data.slice();
 
+    // function to project our state to something the library understands
     var prepare_data = function() {
         var result = [];
 
@@ -19,8 +22,11 @@ block.fn.rolling_chart = function(config) {
         return [result];
     };
 
+    // initial setup of library state (also builds necessary HTML)
     var plot = $.plot(this.$element, prepare_data(), options.chart);
 
+
+    // register actions for this block
     this.actions({
         'add': function(e, message) {
             // roll memory
@@ -28,8 +34,10 @@ block.fn.rolling_chart = function(config) {
                 data = data.slice(1);
             }
 
+            // update block state
             data.push(message.value);
 
+            // update HTML
             plot.setData(prepare_data());
             plot.setupGrid();
             plot.draw();
