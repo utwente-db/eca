84c0dbf8c64800ff48f81666ea22d287bdd7072b
Brend Wanders <b.wanders@utwente.nl>
2014-09-22 15:50:25 +0200

    Fixes timing issues for wordcloud rerender.

--- template_static/lib/wordcloud.js
+++ template_static/lib/wordcloud.js
@@ -2,17 +2,15 @@
     // a simple wordcloud example
     block.fn.wordcloud = function(config) {
         var options = $.extend({
-                        // weight=0 means word is not in cloud
-                        weight_function : function(val,max) { return val; },
+            // weight=0 means word is not in cloud
+            weight_function : function(val,max) { return val; },
         }, config);
 
         var $container = $(this.$element);
-        // create empty wordcloud with parameter options
-
-        var wordcloud_el = $container.jQCloud([{}]);
 
         // dict containing the labels and values
         var worddata_dict = {};
+        var dirty = false;
 
         var addword = function(label, value) {
             if (worddata_dict.hasOwnProperty(label)) {
@@ -20,23 +18,33 @@
             } else {
                 worddata_dict[label] = value;
             }
-            redraw();
+            flag_dirty();
         }
 
         var setword = function(label, value) {
             worddata_dict[label] = value;
-            redraw();
+            flag_dirty();
+        }
+
+        var flag_dirty = function() {
+            dirty = true;
         }
 
         var redraw = function() {
+            if(!dirty) {
+                window.setTimeout(redraw, 500);
+                return;
+            }
+
             var result = [];
             var max = 0;
-            // incomplete, determine max
+
             for (var k in worddata_dict) {
                 if (worddata_dict.hasOwnProperty(k)) {
                         max = Math.max(max, worddata_dict[k]);
                 }
             }
+
             for (var k in worddata_dict) {
                 if (worddata_dict.hasOwnProperty(k)) {
                     var w = options.weight_function(worddata_dict[k],max);
@@ -44,7 +52,10 @@
                         result.push({text: k, weight: w});
                 }
             }
-            $($container).empty().jQCloud(result);
+            $($container).empty().jQCloud(result, {delayedMode: false});
+
+            dirty = false;
+            window.setTimeout(redraw, 500);
         }
 
         var reset = function() {
@@ -62,6 +73,10 @@
                 reset();
             }
         });
+
+        // start redraw loop
+        window.setTimeout(redraw, 500);
+
         // return element to allow further work
         return this.$element;
     }
