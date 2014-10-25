fb15f1d1393ebbd88bf1333cd63ae81133a0d101
Brend Wanders <b.wanders@utwente.nl>
2014-10-25 12:43:16 +0200

    Adds tweet to ARFF converter to feed data to WEKA.

---
+++ tweet2arff.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+import eca.arff
+
+def rows(tweets):
+    """
+    This Generator function takes an opened data file with one JSON object
+    representing a tweet per line, and generates a row for each tweet.
+    """
+    for line in tweets:
+        tweet = json.loads(line)
+        yield {'tweet': tweet['text']}
+
+def main():
+    """
+    Main program entry point.
+    """
+    parser = argparse.ArgumentParser(description='Tweet data to ARFF converter')
+    parser.add_argument('file', type=argparse.FileType('r'), help='Twitter data source')
+    parser.add_argument('output', type=argparse.FileType('w'), help='Output file')
+    args = parser.parse_args()
+
+    # attribute description
+    fields = [
+        eca.arff.Field('tweet', eca.arff.Text()),
+        eca.arff.Field('@@class@@', eca.arff.Nominal(['a','b','c']))
+    ]
+
+    # create item generator
+    items = rows(tweets)
+
+    eca.arff.save(args.output, fields, items)
+
+if __name__ == '__main__':
+    main()
