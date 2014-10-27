a673bb1eaba6a3ad83ae900353c4d594f651a587
Brend Wanders <b.wanders@utwente.nl>
2014-10-27 10:46:39 +0100

    Fixes encoding woes for windows.
    This version always uses utf-8 as output encoding, allowing easier use
    on platforms that use lacking default encodings.
    
    Note that if only Python 3.4 would be supported, this could've easily been
    done through the argparse.FileType's encoding parameter. Right now the
    output_file_type function emulates the desired behavior in a non-generic
    way.

--- tweet2arff.py
+++ tweet2arff.py
@@ -5,6 +5,17 @@ import json
 
 import eca.arff
 
+def output_file_type(name):
+    """Acts as an output file type for argparse. Always uses utf-8 encoding."""
+    if name == '-':
+        import sys
+        return sys.stdout
+
+    try:
+        return open(name, 'w', encoding='utf-8')
+    except OSError as e:
+        raise argparse.ArgumentTypeError("can't open '{}': {}".format(name, e))
+
 def rows(tweets):
     """
     This Generator function takes an opened data file with one JSON object
@@ -20,7 +31,7 @@ def main():
     """
     parser = argparse.ArgumentParser(description='Tweet data to ARFF converter')
     parser.add_argument('file', type=argparse.FileType('r'), help='Twitter data source')
-    parser.add_argument('output', type=argparse.FileType('w'), help='Output file')
+    parser.add_argument('output', type=output_file_type, help='Output file')
     args = parser.parse_args()
 
     # attribute description
